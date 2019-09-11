ee4f38c634bb497d2d9d8ed3bbe0507ec9a84226
Jhon112 <jhonarmont@hotmail.com>
2019-09-11 04:26:49 +0000

    13. Insert in sorted linked list mandatory Technical interview preparation:
    You are not allowed to google anything
    Whiteboard first
    Write a function in C that inserts a number into a sorted singly linked list.
    
    Prototype: listint_t *insert_node(listint_t **head, int number);
    Return: the address of the new node, or NULL if it failed

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_node - adds a number into a sorted singly linked list
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be included in list
+ * Return: address of the new node or NULL if it fails
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *current, *next_node;
+
+	current = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (current->next != NULL)
+		{
+			next_node = current->next;
+			if (next_node->n > new->n)
+			{
+				new->next = current->next;
+				current->next = new;
+				break;
+			}
+			current = current->next;
+		}
+	}
+	return (new);
+}
