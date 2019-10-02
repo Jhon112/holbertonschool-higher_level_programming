919687eadf7646a66e3c9331acf5972a509a57ee
jhon112 <jhonarmont@hotmail.com>
2019-10-02 02:35:53 +0000

    Add documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/u2sr/bin/python3
+""" Declaration of Square class """
+
 class Square():
     """Declare protected attribue"""
 
     def __init__(self, size):
+        """__init__ method to declarate private attr
+
+        Args:
+            size (int): size of square
+
+        """
         self.__size = size
