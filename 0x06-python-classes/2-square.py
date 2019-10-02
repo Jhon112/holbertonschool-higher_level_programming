444d99ae5d2a137295250faecef4ddb97ffa3c67
jhon112 <jhonarmont@hotmail.com>
2019-10-02 04:05:45 +0000

    Split validations

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,9 +17,10 @@ class Square():
             size (int): size of square
 
         """
-        if not isinstance(size, (int, float)):
+        if not isinstance(size, int):
             raise TypeError("Size must be and integer")
-        elif size < 0:
+
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+
+        self.__size = size
