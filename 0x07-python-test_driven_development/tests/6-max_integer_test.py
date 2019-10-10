a3a7113336ad7c70e7e56f8fa1d572281e35d904
jhon112 <jhonarmont@hotmail.com>
2019-10-10 01:34:22 +0000

    Add new tests. List with only one element, with max at end, at beggining and at middle

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -19,12 +19,28 @@ class TestMaxInteger(unittest.TestCase):
 
         self.assertIs(max_integer([]), None)
 
-    def test_returns_correct_value(self):
+    def test_max_at_end(self):
         """
         Test if it returns the correct value (max integer of an int list
         """
 
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_at_beggining(self):
+        """test if returns max value when it's at first position"""
+
+        self.assertEqual(max_integer([4,3,2]), 4)
+
+    def test_max_at_middle(self):
+        """test if returns max value when it's at mid position"""
+
+        self.assertEqual(max_integer([3, 2, 4, 1, 0]), 4)
+
+
+    def test_unique_element(self):
+        """Test list with only one element"""
+
+        self.assertEqual(max_integer[4]), 4)
 
     def test_negative_numbers(self):
         """
