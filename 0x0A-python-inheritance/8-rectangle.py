8dfc3e14febb6874f242a655484c985d77214415
jhon112 <jhonarmont@hotmail.com>
2019-10-16 05:16:49 +0000

    Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
    Instantiation with width and height: def __init__(self, width, height):
    width and height must be private. No getter or setter
    width and height must be positive integers, validated by integer_validator

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""contains class Rectangle that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """class that defines a rectangle
+
+    Attributes:
+        width (int): width of rectangle
+        height (int): height of rectangle
+
+    """
+
+    def __init__(self, width, height):
+        """
+        init method for class
+
+        Args:
+            width (int): width to be set
+            height (int): height to be set
+        """
+        if self.integer_validator("width", width) is True:
+            self.__width = width
+        if self.integer_validator("height", height) is True:
+            self.__height = height
