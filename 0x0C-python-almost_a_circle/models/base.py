5c48f1a50d71c8674e97ecc81fdb6478cc5a23a6
Jhon Arias <jhonarmont@hotmail.com>
2019-10-21 15:46:01 -0500

    Add docstring to init method in base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,17 +4,24 @@
 
 class Base:
     """
-    This class will be the “base” of all other
+    This class is the “base” of all other
     classes in this project. The goal of it is to manage
     id attribute in all your future classes
 
     Attributes:
         __nb_objects (int): number of instances
+        id (int): id of instance. id if arg passed, __nb_obj otherwise
     """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Init method for class
+
+        Args:
+            id (int): id of instance. It's never the same
+        """
         if not id:
             Base.__nb_objects += 1
             id = Base.__nb_objects
