3840df88e41f010b7afa12dc0ad73b084de30dbe
Jhon Arias <jhonarmont@hotmail.com>
2019-10-21 21:46:10 -0500

    Add method to test area method
    Remove duplicate tests to make the file shorter

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,8 +1,9 @@
 import unittest
 from models.rectangle import Rectangle
 
+
 class testRectangleMethods(unittest.TestCase):
-    
+
     def test_all(self):
         r1 = Rectangle(10, 2, 20, 30)
         #test id
@@ -36,16 +37,12 @@ class testRectangleMethods(unittest.TestCase):
         r1.y = 35
         self.assertEqual(r1.y, 35)
 
+    def test_area(self):
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
+
     def test_height(self):
         r3 = Rectangle(10, 2)
-        self.assertEqual(r3.id, 2)
-
-        # Check getter works
-        self.assertEqual(r3.height, 2)
-
-        # check setter works
-        r3.height = 5
-        self.assertEqual(r3.height, 5)
 
         # check an exception is raised when trying to access
         # a private attribute
@@ -61,15 +58,7 @@ class testRectangleMethods(unittest.TestCase):
             r3.height = 0
 
     def test_width(self):
-        r2 = Rectangle(10, 2, id=12)
-        self.assertEqual(r2.id, 12)
-
-        # Check getter works
-        self.assertEqual(r2.width, 10)
-
-        # check setter works
-        r2.width = 5
-        self.assertEqual(r2.width, 5)
+        r2 = Rectangle(10, 2)
 
         # check an exception is raised when trying to access
         # a private attribute
@@ -87,12 +76,6 @@ class testRectangleMethods(unittest.TestCase):
     def test_x(self):
         # Check getter works
         r4 = Rectangle(10, 2)
-        self.assertEqual(r4.id, 3)
-        self.assertEqual(r4.x, 0)
-
-        # check setter works
-        r4.x = 5
-        self.assertEqual(r4.x, 5)
 
         # check an exception is raised when trying to access
         # a private attribute
@@ -109,14 +92,6 @@ class testRectangleMethods(unittest.TestCase):
 
     def test_y(self):
         r5 = Rectangle(10, 2)
-        self.assertEqual(r5.id, 4)
-
-        # Check getter works
-        self.assertEqual(r5.y, 0)
-
-        # check setter works
-        r5.y = 5
-        self.assertEqual(r5.y, 5)
 
         # check an exception is raised when trying to access
         # a private attribute
