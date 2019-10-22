ad056e8615dc4c6b267cc96d24966ec6da98e0e5
Jhon Arias <jhonarmont@hotmail.com>
2019-10-22 18:27:19 -0500

    Add square class who inherits from Rectangle class. It calls super to intialize Rectangle Overload str method to display in different format

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,41 @@
+"""
+defines the class square, who inherits from Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class that inherits from Rectangle. It defines a Square
+
+    Attributes:
+        __x (int): x position
+        __y (int): y position
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Init method for class. It calls the super function to initialize
+        Rectangle
+
+        The width and height must be assigned to the value of size
+
+        All width, height, x and y validation inherit from Rectangle
+        - same behavior in case of wrong data
+
+        Args:
+            size (int): width and height of square
+            x (int): x position of square
+            y (int): y position of square
+            id (int): id of square
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Printable form of class
+        """
+
+        args = (self.id, self.x, self.y, self.width)
+        return '[Square] ({}) {}/{} - {}'.format(*args)
