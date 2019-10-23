686634c043ee72480b1c96a2ec5c2d6b34ab9baf
Jhon Arias <jhonarmont@hotmail.com>
2019-10-23 00:18:53 -0500

    add save to file class method to write the JSON string representation of list_objs to a file.
    It open the file, then iterates trough listof objs and gets the dictionary of each obj, then it calls the static method to_json_string to convert the list to json and write it to the file opened

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,9 +32,37 @@ class Base:
     def to_json_string(list_dictionaries):
         """
         returns the JSON string representation of list_dictionaries:
+
+        Args:
+            list_dictionaries (list): list of dictionaries to convert
+
+        return:
+            json representation of list_dictionaries
         """
 
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
-            return json.dumps(list_dictionaries, skipkeys=True)
+            return json.dumps(list_dictionaries, skipkeys=True, indent=2)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file:
+
+        list_objs is a list of instances who inherits of Base
+        If list_objs is None, save an empty list
+        The filename must be: <Class name>.json
+        use the static method to_json_string (created before)
+        overwrite the file if it already exists
+        """
+
+        with open("{}.json".format(cls.__name__), mode="w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = []
+                for obj in list_objs:
+                    list_dicts.append(obj.to_dictionary())
+                json_list_dicts = Base.to_json_string(list_dicts)
+                file.write(json_list_dicts)
