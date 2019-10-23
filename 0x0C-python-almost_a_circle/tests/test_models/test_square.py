010a305c8a5f50dc8b65c8b619299549f41026e7
Jhon Arias <jhonarmont@hotmail.com>
2019-10-22 23:10:11 -0500

    Add tests for setters and getters

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -8,9 +8,6 @@ class TestSquareMethods(unittest.TestCase):
     def test_square(self):
         s1 = Square(5)
 
-        # Check id
-        self.assertEqual(s1.id, 10)
-
         # check width and height are the same
         self.assertEqual(s1.width, 5)
         self.assertEqual(s1.height, 5)
@@ -20,9 +17,17 @@ class TestSquareMethods(unittest.TestCase):
 
         # check an error is raised when trying to
         # intialize with a non integer val 
-        
         with self.assertRaises(TypeError, msg="height must be an integer"):
             s1.height = 'hi'
 
         with self.assertRaises(TypeError, msg="height must be an integer"):
             s2 = Square("hello")
+
+        # teset getter
+        s1.size = 8
+        self.assertEqual(s1.height, 8)
+         # test size.setter
+        with self.assertRaises(TypeError, msg="width must be an integer"):
+            s1.size = "hello"
+        with self.assertRaises(ValueError, msg="width must be > 0"):
+            s1.size = -5
