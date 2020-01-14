f2fdfec60b0fd5af924ad385f49f07cafd4ca76d
Jhon Arias <jhonarmont@hotmail.com>
2020-01-13 23:44:24 -0500

    Change text format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,5 @@ import urllib.request
 
 with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
     r = response.read()
-    print("""Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".format(type(r), r, r.decode('utf-8'))
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(type(r), r, r.decode('utf-8'))
     )
