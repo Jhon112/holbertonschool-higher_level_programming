dc516efc3273f2f855af312871ee449966541f33
Jhon Arias <jhonarmont@hotmail.com>
2020-01-14 02:50:41 -0500

    Write a Python script that takes in a string and sends a search request to the Star Wars API

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+fetches data from starwars api
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://swapi.co/api/people/"
+    params = {'search': sys.argv[1]}
+    r = requests.get(url, params=params)
+    try:
+        json = r.json()
+        print('Number of results: ', json['count'])
+        for obj in json['results']:
+            print(obj['name'])
+    except ValueError as err:
+        print('Not a valid JSON')
