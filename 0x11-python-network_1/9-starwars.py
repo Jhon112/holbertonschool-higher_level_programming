ac9028227c9f45465fe2c633468f0551c73a79b4
Jhon Arias <jhonarmont@hotmail.com>
2020-01-14 03:03:25 -0500

    Remove try exception

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -10,10 +10,7 @@ if __name__ == "__main__":
     url = "https://swapi.co/api/people/"
     params = {'search': sys.argv[1]}
     r = requests.get(url, params=params)
-    try:
-        json = r.json()
-        print('Number of results: ', json['count'])
-        for obj in json['results']:
-            print(obj['name'])
-    except ValueError as err:
-        print('Not a valid JSON')
+    json = r.json()
+    print('Number of results: ', json['count'])
+    for obj in json['results']:
+        print(obj['name'])
