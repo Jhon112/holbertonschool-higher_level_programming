b793dca0798f68369a91f94d370d1aa4797614af
Jhon Arias <jhonarmont@hotmail.com>
2020-01-14 01:19:56 -0500

    Handle error codes > 400

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,8 +8,8 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    try:
-        r = requests.get(url).raise_for_status()
+    r = requests.get(url)
+    if r:
         print(r.text)
-    except requests.exceptions.RequestException as error:
-        print("Error code:", error.response.status_code)
+    else:
+        print("Error code:", r.status_code)
