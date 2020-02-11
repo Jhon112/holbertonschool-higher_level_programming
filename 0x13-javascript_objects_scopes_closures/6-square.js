eafdc25ab014171a38c8540ec61b2926e8b5e15f
Jhon Arias <jhonarmont@hotmail.com>
2020-02-11 01:20:41 -0500

    Create an instance method called charPrint(c) that prints the rectangle using the character c If c is undefined, use the character X

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      let index = 0;
+      while (index < this.height) {
+        let str = "";
+        for (let row = 0; row < this.width; row++) {
+          str += c;
+        }
+        console.log(str);
+        index++;
+      }
+    } else {
+      super.print();
+    }
+  };
+};
