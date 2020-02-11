60129de444b4b2acc2e1dd51f02291848dd7539a
Jhon Arias <jhonarmont@hotmail.com>
2020-02-11 01:36:50 -0500

    Remove constructor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,6 @@
 const Square5 = require('./5-square');
 
 module.exports = class Square extends Square5 {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c) {
       let index = 0;
