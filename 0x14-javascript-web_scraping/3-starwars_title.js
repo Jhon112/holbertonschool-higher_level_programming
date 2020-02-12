2995ecbd43525575aa4b7af1e74a90488144a6d8
Jhon Arias <jhonarmont@hotmail.com>
2020-02-11 22:32:00 -0500

    Fix linter

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,6 @@ const request = require('request');
 const url = `http://swapi.co/api/films/${process.argv[2]}`;
 
 request(url, (error, response, body) => {
+  if (error) throw error;
   console.log(JSON.parse(body).title);
 });
-
