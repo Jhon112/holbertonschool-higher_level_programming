ded29a895db50d1230195f7b6205bffa6af014c9
Jhon Arias <jhonarmont@hotmail.com>
2020-02-11 22:50:18 -0500

    PRints number of moviews where a specific character was present

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const results = JSON.parse(body).results;
+    let counter = 0;
+
+    for (const fiml of results) {
+      if (isOnFilm(fiml)) counter++;
+    }
+
+    console.log(counter);
+  }
+});
+
+function isOnFilm(film) {
+  let characters = film.characters;
+  return characters.includes('https://swapi.co/api/people/18/');
+}
